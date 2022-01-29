443f31a745af6d1542e1bfc4d2e3516310d1c022
Sergiy Zhitaryuk <zitaruksergij@gmail.com>
2022-01-29 19:39:57 +0200

    Add log graph

--- src/App.js
+++ src/App.js
@@ -27,7 +27,14 @@ function App() {
     }
   ]);
 
-  useEffect(async () => { await fetch('/dataArray.json').then((response) => response.json().then((dataItem) => setDataOrigin(dataItem))) }, []);
+  useEffect(async () => {
+    await fetch('/dataArray.json')
+        .then((response) => {
+          response.json().then((dataItem) => {
+            setDataOrigin(dataItem)
+          })
+        })
+  }, []);
   // useEffect(async () => { await fetch('/wp-json/irc/v2/liquidation').then((response) => response.json().then((dataItem) => setDataOrigin(dataItem))) }, []);
 
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -50,31 +57,66 @@ function App() {
       case 6:
         return array
       case 7:
-        console.log("1111. " + moment().diff(new Date(dateFilterStart)), 'days')
-        console.log("2222. " + moment().diff(new Date(array[200].date)), 'days')
-        console.log("3333. " + new Date(array[200].date))
-        console.log("3333. " + moment().diff(new Date(array[200].date)), 'days')
-        console.log("..... ")
-        console.log(moment().diff(new Date(array[200].date)), 'days')
+        return array
+      case 8:
         return array.slice().filter(item =>
-          (dateFilterEnd ? moment().diff(dateFilterEnd) <= moment().diff(item.date) : item.date) && (dateFilterStart ? moment().diff(dateFilterStart) >= moment().diff(item.date) : item.date)
+            (dateFilterEnd ? moment().diff(dateFilterEnd) <= moment().diff(item.date) : item.date) && (dateFilterStart ? moment().diff(dateFilterStart) >= moment().diff(item.date) : item.date)
         )
+
     }
   }
 
   function numFormatter(value) {
     let num = value;
     if (num < 0) { num = Math.abs(num) }
-    var suffixes = ["", "K", "M", "B", "T"];
-    var suffixNum = Math.floor(("" + num).length / 3);
-    var shortValue = parseFloat((suffixNum != 0 ? (num / Math.pow(1000, suffixNum)) : num).toPrecision(2));
+    let suffixes = ["", "K", "M", "B", "T"],
+        suffixNum = Math.floor(("" + num).length / 3),
+        shortValue = parseFloat((suffixNum != 0 ? (num / Math.pow(1000, suffixNum)) : num).toPrecision(2));
     return "$" + shortValue + suffixes[suffixNum];
   }
 
+  dataOrigin.btc_currency = typeof dataOrigin.btc_currency === 'undefined' ? [] : dataOrigin.btc_currency;
+
   const option = {
-    color: ['#e0294a', '#41c390', '#d8a70d'],
+    color: [
+      '#e0294a',
+      '#41c390',
+      '#d8a70d'
+    ],
     tooltip: {
       trigger: 'axis',
+      formatter: function ( param ) {
+        let $template = '',
+            $title;
+        param.forEach(($item, $key) => {
+          let $value;
+          switch ($key) {
+            case 0 :
+              $value = dataOrigin.btc_liquidation_short[$item.dataIndex].value;
+            break;
+            case 1 :
+              $value = dataOrigin.btc_liquidation_long[$item.dataIndex].value;
+            break;
+            case 2 :
+              $value = dataOrigin.btc_currency[$item.dataIndex].value;
+            break;
+          }
+          if($key === 0) {
+
+          }
+          $title = '<div>' + $item.name + '</div>';
+          $template += '<div class="rowTitle">';
+          $template += $item.marker;
+          $template += $item.seriesName;
+          $template += '<b>' + (Math.abs($value)).toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }) + '</b>';
+          $template += '</div>';
+        });
+
+        return $title + $template;
+      },
       axisPointer: {
         type: 'cross',
         crossStyle: {
@@ -87,21 +129,36 @@ function App() {
     },
     toolbox: {
       feature: {
-        dataView: { show: false, readOnly: false },
-        magicType: { show: false, type: ['line', 'bar'] },
-        restore: { show: false },
-        saveAsImage: { show: true }
+        dataView: {
+          show: false,
+          readOnly: false
+        },
+        magicType: {
+          show: false,
+          type: ['line', 'bar']
+        },
+        restore: {
+          show: false
+        },
+        saveAsImage: {
+          show: true
+        }
       }
     },
     legend: {
-      data: ['Shorts', 'Longs', 'BTC Price']
+      data: [
+        'Shorts',
+        'Longs',
+        'BTC Price'
+      ]
     },
     xAxis: [
       {
         type: 'category',
-        data: dataOrigin?.btc_currency ? reverceArrayFun(dataOrigin.btc_currency).map(itemI =>
-          (`${itemI.date.split("-")[2] + " " + months[Number(itemI.date.split("-")[1]) - 1] + " " + itemI.date.split("-")[0]}`)
-        ) : [],
+        data: reverceArrayFun(dataOrigin.btc_currency).map(itemI => {
+                return (`${itemI.date.split("-")[2] + " " + months[Number(itemI.date.split("-")[1]) - 1] + " " + itemI.date.split("-")[0]}`)
+              })
+        ,
         axisPointer: {
           type: 'shadow'
         }
@@ -122,20 +179,26 @@ function App() {
         name: 'Shorts',
         type: 'bar',
         stack: 'one',
-        data: dataOrigin?.btc_liquidation_short ? reverceArrayFun(dataOrigin.btc_liquidation_short).map(itemI => -itemI.value) : [],
+        data: dataOrigin?.btc_liquidation_short ? reverceArrayFun(dataOrigin.btc_liquidation_short).map(itemI => {
+          return tabIndex === 7 ? -Math.log10(itemI.value) : -itemI.value
+        }) : [],
       },
       {
         name: 'Longs',
         type: 'bar',
         stack: 'one',
-        data: dataOrigin?.btc_liquidation_long ? reverceArrayFun(dataOrigin.btc_liquidation_long).map(itemI => itemI.value) : [],
+        data: dataOrigin?.btc_liquidation_long ? reverceArrayFun(dataOrigin.btc_liquidation_long).map(itemI => {
+          return tabIndex === 7 ? Math.log10(itemI.value) : itemI.value
+        }) : [],
       },
       {
         name: 'BTC Price',
         type: 'line',
         yAxisIndex: 1,
         showSymbol: false,
-        data: dataOrigin?.btc_currency ? reverceArrayFun(dataOrigin.btc_currency).map(itemI => Number(itemI.value)) : [],
+        data: dataOrigin?.btc_currency ? reverceArrayFun(dataOrigin.btc_currency).map(itemI => {
+          return Number(tabIndex === 7 ? Math.log10(itemI.value) : itemI.value)
+        }) : [],
       }
     ]
   };
@@ -162,40 +225,50 @@ function App() {
   };
 
   return (
-    <div className='containerCal'>
-      <div className='wrapFilters'>
-        <Tabs selectedIndex={tabIndex} onSelect={index => { if (index !== 7) { setTabIndex(index) } else { setTabIndex(index); setOpenModal(true); } }}>
-          <TabList>
-            <Tab>7D</Tab>
-            <Tab>1M</Tab>
-            <Tab>3M</Tab>
-            <Tab>1Y</Tab>
-            <Tab>2Y</Tab>
-            <Tab>3Y</Tab>
-            <Tab>All</Tab>
-            <Tab>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="20px" width="20px" viewBox="0 0 24 24"  >
-                <path fillRule="evenodd" clipRule="evenodd" d="M15 5H9V4C9 3.44772 8.55228 3 8 3C7.44772 3 7 3.44772 7 4V5H5C3.89543 5 3 5.89543 3 7V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V7C21 5.89543 20.1046 5 19 5H17V4C17 3.44772 16.5523 3 16 3C15.4477 3 15 3.44772 15 4V5ZM6 7C5.44772 7 5 7.44771 5 8V10H19V8C19 7.44772 18.5523 7 18 7H17C17 7.55228 16.5523 8 16 8C15.4477 8 15 7.55228 15 7H9C9 7.55228 8.55228 8 8 8C7.44772 8 7 7.55228 7 7H6ZM19 12H5V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V12Z"></path>
-              </svg>
-            </Tab>
-          </TabList>
-        </Tabs>
-      </div>
-      <Modal isOpen={openModal} onRequestClose={() => setOpenModal(false)} style={customStyles}  >
+      <div className='containerCal'>
+        <div className='wrapFilters'>
+          <Tabs selectedIndex={tabIndex} onSelect={
+            index => {
+              if (index !== 8) {
+                setTabIndex(index)
+              } else {
+                setTabIndex(index);
+                setOpenModal(true);
+              }
+            }
+          }>
+            <TabList>
+              <Tab>7D</Tab>
+              <Tab>1M</Tab>
+              <Tab>3M</Tab>
+              <Tab>1Y</Tab>
+              <Tab>2Y</Tab>
+              <Tab>3Y</Tab>
+              <Tab>All</Tab>
+              <Tab>LOG</Tab>
+              <Tab>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="20px" width="20px" viewBox="0 0 24 24"  >
+                  <path fillRule="evenodd" clipRule="evenodd" d="M15 5H9V4C9 3.44772 8.55228 3 8 3C7.44772 3 7 3.44772 7 4V5H5C3.89543 5 3 5.89543 3 7V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V7C21 5.89543 20.1046 5 19 5H17V4C17 3.44772 16.5523 3 16 3C15.4477 3 15 3.44772 15 4V5ZM6 7C5.44772 7 5 7.44771 5 8V10H19V8C19 7.44772 18.5523 7 18 7H17C17 7.55228 16.5523 8 16 8C15.4477 8 15 7.55228 15 7H9C9 7.55228 8.55228 8 8 8C7.44772 8 7 7.55228 7 7H6ZM19 12H5V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V12Z"></path>
+                </svg>
+              </Tab>
+            </TabList>
+          </Tabs>
+        </div>
+        <Modal isOpen={openModal} onRequestClose={() => setOpenModal(false)} style={customStyles}  >
 
-        <DatePicker selected={stateCal} onChange={handleOnChange} />
+          <DatePicker selected={stateCal} onChange={handleOnChange} />
 
 
-        {/* <DateRange
+          {/* <DateRange
           onChange={handleOnChange}
           months={1}
           ranges={stateCal}
           direction="vertical"
         /> */}
-        <Button variant="contained" onClick={() => { setOpenModal(false); setDataOrigin(dataOrigin) }}>Done</Button>
-      </Modal>
-      <ReactECharts option={option} />
-    </div >
+          <Button variant="contained" onClick={() => { setOpenModal(false); setDataOrigin(dataOrigin) }}>Done</Button>
+        </Modal>
+        <ReactECharts option={option} />
+      </div >
   )
 }
 
